cd8caf9ce5416801ad2055d7ca74a4d4afd27918
CoolCode2 <wcole2222@gmail.com>
2017-04-21 09:19:06 -0600

    first commit routes built app.js started need to move api key to back end and break routes into routes and controllers

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+console.log("Twerkin");
+
+$(document).ready(function(){
+
+
+//////// ajax get route to the backend first  triggers the matching router 
+// goes to the macthing controller requesting with node (hw) request is in the controller res
+// need to install and require request
+
+var randoQ;
+	$('#rando').on("click", function(){
+		console.log("clike");
+		var randoQ = $.get('https://quotes.rest/quote/random.json?api_key=')
+			.done(function(data){
+				console.log(data);
+				$('.qbox').append(data.contents.quote);
+				$('.abox').append(data.contents.author);
+			});
+	});
+
+	$("#save").on("click", function(){
+		console.log('save clik');
+		var q =  $('.qbox').text();
+		var a =  $('.abox').text();
+		
+		$.ajax({
+		
+			url:'http://localhost:3000/api/quotes',
+			type: 'POST',
+			dataType:"json",
+			data: {_id: 16, quote: q, author: a},
+			ContentType:"application/json"
+            }).done(function(res){
+			console.log(res);
+			$.get('/api/quotes', function(res){
+				console.log(res);
+				res.forEach(function(res){
+					$(".saveBox").append(res);
+				});
+			});
+		});
+
+	});
+
+
+
+});
--- server.js
+++ server.js
@@ -9,22 +9,38 @@ app.use(bodyParser.json());// we use express to parse  json obect requests
 /************
  * DATABASE *
  ************/
+app.use(express.static('public'));
 
-/////serve up the hompage
-app.get('/', function homepage(req,res){
-	res.sendFile(__dirname + '/views/index.html');
-});
+
 
 
 app.use(express.static(__dirname + '/public'));//serve static files from the public folder that the user can see
 
+// ///serves up home page
+// app.get('/', function homepage(req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+// define a root route: localhost:3000/
+app.get('/', function homepage (req, res) {
+  res.sendfile(__dirname + '/views/index.html');
+});
+
+
+var apiKey= require('./env/api_env.js');
+
+var start = 'http://google.com',
+	next = '/images/';
+
+	var url = start + next + apiKey;
+
+console.log(apiKey);
+console.log(url);
 
 
 
-
-
-/////INDEX ROUTE WORKING +++++++++++++++++++++++++++++
-app.get('/api/quotes', function album_index(req,res){
+/////INDEX ROUTE WORKING +++++++++++++++++++++++++++++++
+app.get('/api/quotes', function quotes_index(req,res){
 	db.Quote.find({}, function (err,DBquotes){
 		if (err){
 			console.log(err);
@@ -33,17 +49,39 @@ app.get('/api/quotes', function album_index(req,res){
 	});
 });
 
-/////
-app.get('/api/quotes/:id', function albumFindOne(req,res){
-		
-		
+///// findONE Route WORKING +++++++++++++++++++++++++++++++
+app.get('/api/quotes/:id', function quotes_FindOne(req,res){
 		
 	db.Quote.findOne({_id: req.params.id}, function(err, DBquotes){
 		res.json(DBquotes);
 	});
 });
 
+//post a new Q //WORKING creates a new quote////////// working
+app.post('/api/quotes', function postQ(req,res){
+        //res.json("JSON req.body._id:   "+ req.body._id);
+  var postQ = new db.Quote
+    ({
+    _id: req.body._id,
+    quote: req.body.quote,
+    author: req.body.author
+      });
+      
+    postQ.save(function(err, q){
+          res.json(q);
+  });
+  
+});
+
+///////////////// DELETE /////////////////////// Working
+app.delete('/api/quotes/:id', function(req,res){
+	var deleteID = req.params.id;
+	db.Quote.findOneAndRemove({_id: deleteID}, function(err, cut){
+		res.json(cut+" was taken out");
+	});
+});
+
 
 app.listen(3000, function(){
-	console.log('3000 server is up on http://localhost:4000');
+	console.log('3000 server is up on http://localhost:3000');
 });
